bec90317c2742be4e7a5f084565568bd5016e6c7
marianne20106 <marianne.nikkila@student.hamk.fi>
2021-02-18 06:01:37 +0200

    php

---
+++ yhteydenotto.php
@@ -0,0 +1,66 @@
+<?php
+include "aboutus.html";
+?>
+<?php
+
+if (isset($_POST["name"])){
+    $name=$_POST["name"];
+}
+else{
+    $name="";
+}
+
+if (isset($_POST["email"])){
+    $email=$_POST["email"];
+}
+else{
+    $email="";
+}
+
+if (isset($_POST["message"])){
+    $message=$_POST["message"];
+}
+else{
+    $message="";
+}
+
+  
+  $yhteys = mysqli_connect("shell.hamk.fi", "trtkp20a3", "trtkp20a3passwd");
+  
+  // Check connection
+  if (!$yhteys) {
+    die("Yhteyden muodostaminen epäonnistui: " . mysqli_connect_error());
+  }
+  echo "Yhteys OK."; // debuggia
+  
+  $tietokanta=mysqli_select_db($yhteys, "trtkp20a3");
+  if (!$tietokanta) {
+    die("Tietokannan valinta epäonnistui: " . mysqli_connect_error());
+  }
+  echo "Tietokanta OK."; // debuggia
+  
+
+  //Jos lomakkeelta saatiin oheiset tiedot
+  //Tallennetaan tietokantaan käyttäen prepared statementtia - ei tarvitse 
+  //välittää vihamielisistä syötteistä
+  if ($name && $email && $message){
+    $sql="insert into yhteyden_otot(Name, Email, Message) values(?,?,?)";
+    $stmt=mysqli_prepare($yhteys, $sql);
+	if(!$stmt) {
+      echo "Virhe: ".mysqli_error($yhteys);
+    }
+    mysqli_stmt_bind_param($stmt, 'sss', $name, $email, $message);
+    mysqli_stmt_execute($stmt);
+    $last_id = mysqli_insert_id($yhteys);
+    mysqli_stmt_close($stmt);
+  
+ 
+ $stmt=mysqli_prepare($yhteys, $sql); 
+
+  }
+  //Yhteys katkeaa automaattisesti, kun scripti loppuu, mutta tässä kuitenkin 	
+  mysqli_close($yhteys); 
+  
+ 
+  exit;
+  ?>
