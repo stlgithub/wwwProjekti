41f73c4011abcdb87ecb976d5e7ffe6b416fcb3e
marianne20106 <marianne.nikkila@student.hamk.fi>
2021-02-22 18:34:45 +0200

    Update yhteydenotto.php

--- yhteydenotto.php
+++ yhteydenotto.php
@@ -1,6 +1,12 @@
 <?php
 include "aboutus.html";
 ?>
+
+<?php
+session_start();
+require_once "config.php";
+?>
+
 <?php
 
 if (isset($_POST["name"])){
@@ -24,40 +30,19 @@ else{
     $message="";
 }
 
-  
-  $yhteys = mysqli_connect("localhost", "trtkp20a3", "trtkp20a3passwd");
-  
-  // Check connection
-  if (!$yhteys) {
-    die("Yhteyden muodostaminen epäonnistui: " . mysqli_connect_error());
-  }
-  echo "Yhteys OK."; 
-  
-  $tietokanta=mysqli_select_db($yhteys, "trtkp20a3");
-  if (!$tietokanta) {
-    die("Tietokannan valinta epäonnistui: " . mysqli_connect_error());
-  }
-  echo "Tietokanta OK."; 
-  
+   
 
 
-  if ($name && $email && $message){
+  
     $sql="insert into marianne_yhteydenotot(Name, Email, Message) values(?,?,?)";
-    $stmt=mysqli_prepare($yhteys, $sql);
-	if(!$stmt) {
-      echo "Virhe: ".mysqli_error($yhteys);
-    }
+    $stmt=mysqli_prepare($mysqli, $sql);
     mysqli_stmt_bind_param($stmt, 'sss', $name, $email, $message);
     mysqli_stmt_execute($stmt);
-    $last_id = mysqli_insert_id($yhteys);
     mysqli_stmt_close($stmt);
-  
- 
- $stmt=mysqli_prepare($yhteys, $sql); 
-
-  }
-  
-  mysqli_close($yhteys); 
+	mysqli_close($mysqli);
+	echo '<script type="text/javascript">';
+	echo 'alert("Message sent successfully!")';
+	echo '</script>';
   
  
   exit;
