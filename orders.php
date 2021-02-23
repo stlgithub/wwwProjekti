35f1ada55585dd72479dd3016447b6e1885801ee
Sami Lappalainen <sami.lappalainen@hotmail.fi>
2021-02-23 05:01:22 +0200

    Update orders.php

--- orders.php
+++ orders.php
@@ -1,25 +1,16 @@
 <?php
 session_start();
 require_once "config.php";
+?>
 
+<?php
 $name=$_POST["name"];
 $address=$_POST["address"];
 $total_price=$_POST["total_price"];
 $quantity=$_POST["quantity"];
 $product_name=$_POST["product_name"];
 
-
-if (!$yhteys) {
-	die("Yhteyden muodostaminen epĆ¤onnistui: " . mysqli_connect_error());
-}
-
-$tietokanta=mysqli_select_db($yhteys, "example3");
-
-if (!$tietokanta) {
-	die("Tietokannan valinta epĆ¤onnistui: " . mysqli_connect_error());
-}
-
-$sql="insert into orders(Name, Address, ProductID, Quantity, Value) values(?, ?, ?, ?, ?)";
+$sql="insert into group4_orders(Name, Address, ProductID, Quantity, Value) values(?, ?, ?, ?, ?)";
 $stmt=mysqli_prepare($mysqli, $sql);
 	mysqli_stmt_bind_param($stmt, 'sssss', $name, $address, $product_name, $quantity, $total_price);
 	mysqli_stmt_execute($stmt);
